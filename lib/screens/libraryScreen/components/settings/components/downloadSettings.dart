f347e9b75c91948b039cac7281fb977834eba60b
Clashkid155 <frankdiss9@gmail.com>
2020-12-19 05:08:24 +0100

    Fix

--- lib/screens/libraryScreen/components/settings/components/downloadSettings.dart
+++ lib/screens/libraryScreen/components/settings/components/downloadSettings.dart
@@ -19,7 +19,7 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:file_picker/file_picker.dart';
 
 // UI
-import 'package:songtube/ui/dialogs/alertDialog.dart';
+import 'package:songtube/ui/dialogs/alertdialog.dart';
 
 class DownloadSettings extends StatelessWidget {
   @override
@@ -30,80 +30,68 @@ class DownloadSettings extends StatelessWidget {
       icon: EvaIcons.downloadOutline,
       children: <Widget>[
         ListTile(
-          title: Text(
-            Languages.of(context).labelAudioFolder,
-            style: TextStyle(
-              color: Theme.of(context).textTheme.bodyText1.color,
-              fontWeight: FontWeight.w500
+            title: Text(
+              Languages.of(context).labelAudioFolder,
+              style: TextStyle(
+                  color: Theme.of(context).textTheme.bodyText1.color,
+                  fontWeight: FontWeight.w500),
             ),
-          ),
-          subtitle: Text(Languages.of(context).labelAudioFolderJustification,
-            style: TextStyle(fontSize: 12)
-          ),
-          trailing: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Theme.of(context).scaffoldBackgroundColor
-            ),
-            child: IconButton(
-              icon: Icon(EvaIcons.folderOutline, color: Theme.of(context).iconTheme.color),
-              onPressed: () {
-                Permission.storage.request().then((status) {
-                  if (status == PermissionStatus.granted) {
-                    FilePicker.platform.getDirectoryPath().then((path) {
-                      if (path != null) {
-                        config.audioDownloadPath = path;
-                      }
-                    });
-                  }
-                });
-              }
-            )
-          )
-        ),
+            subtitle: Text(Languages.of(context).labelAudioFolderJustification,
+                style: TextStyle(fontSize: 12)),
+            trailing: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Theme.of(context).scaffoldBackgroundColor),
+                child: IconButton(
+                    icon: Icon(EvaIcons.folderOutline,
+                        color: Theme.of(context).iconTheme.color),
+                    onPressed: () {
+                      Permission.storage.request().then((status) {
+                        if (status == PermissionStatus.granted) {
+                          FilePicker.platform.getDirectoryPath().then((path) {
+                            if (path != null) {
+                              config.audioDownloadPath = path;
+                            }
+                          });
+                        }
+                      });
+                    }))),
         ListTile(
-          title: Text(
-            Languages.of(context).labelVideoFolder,
-            style: TextStyle(
-              color: Theme.of(context).textTheme.bodyText1.color,
-              fontWeight: FontWeight.w500
+            title: Text(
+              Languages.of(context).labelVideoFolder,
+              style: TextStyle(
+                  color: Theme.of(context).textTheme.bodyText1.color,
+                  fontWeight: FontWeight.w500),
             ),
-          ),
-          subtitle: Text(Languages.of(context).labelVideoFolderJustification,
-            style: TextStyle(fontSize: 12)
-          ),
-          trailing: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Theme.of(context).scaffoldBackgroundColor
-            ),
-            child: IconButton(
-              icon: Icon(EvaIcons.folderOutline, color: Theme.of(context).iconTheme.color),
-              onPressed: () {
-                Permission.storage.request().then((status) {
-                  if (status == PermissionStatus.granted) {
-                    FilePicker.platform.getDirectoryPath().then((path) {
-                      if (path != null) {
-                        config.videoDownloadPath = path;
-                      }
-                    });
-                  }
-                });
-              }
-            )
-          )
-        ),
+            subtitle: Text(Languages.of(context).labelVideoFolderJustification,
+                style: TextStyle(fontSize: 12)),
+            trailing: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Theme.of(context).scaffoldBackgroundColor),
+                child: IconButton(
+                    icon: Icon(EvaIcons.folderOutline,
+                        color: Theme.of(context).iconTheme.color),
+                    onPressed: () {
+                      Permission.storage.request().then((status) {
+                        if (status == PermissionStatus.granted) {
+                          FilePicker.platform.getDirectoryPath().then((path) {
+                            if (path != null) {
+                              config.videoDownloadPath = path;
+                            }
+                          });
+                        }
+                      });
+                    }))),
         ListTile(
           title: Text(
             Languages.of(context).labelAlbumFolder,
             style: TextStyle(
-              color: Theme.of(context).textTheme.bodyText1.color,
-              fontWeight: FontWeight.w500
-            ),
+                color: Theme.of(context).textTheme.bodyText1.color,
+                fontWeight: FontWeight.w500),
           ),
           subtitle: Text(Languages.of(context).labelAlbumFolderJustification,
-            style: TextStyle(fontSize: 12)
-          ),
+              style: TextStyle(fontSize: 12)),
           trailing: CircularCheckBox(
             activeColor: Theme.of(context).accentColor,
             value: config.enableAlbumFolder,
@@ -113,84 +101,84 @@ class DownloadSettings extends StatelessWidget {
           ),
         ),
         ListTile(
-          title: Text(
-            Languages.of(context).labelDeleteCache,
-            style: TextStyle(
-              color: Theme.of(context).textTheme.bodyText1.color,
-              fontWeight: FontWeight.w500
+            title: Text(
+              Languages.of(context).labelDeleteCache,
+              style: TextStyle(
+                  color: Theme.of(context).textTheme.bodyText1.color,
+                  fontWeight: FontWeight.w500),
             ),
-          ),
-          subtitle: Text(Languages.of(context).labelDeleteCacheJustification,
-            style: TextStyle(fontSize: 12)
-          ),
-          trailing: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Theme.of(context).scaffoldBackgroundColor
-            ),
-            child: IconButton(
-              icon: Icon(EvaIcons.trashOutline, color: Theme.of(context).iconTheme.color),
-              onPressed: () async {
-                double totalSize = 0;
-                String tmpPath = (await getTemporaryDirectory()).path;
-                List<FileSystemEntity> listFiles = Directory(tmpPath).listSync();
-                listFiles.forEach((element) {
-                  if (element is File) {
-                    totalSize += element.statSync().size;
-                  }
-                });
-                totalSize = totalSize * 0.000001;
-                if (totalSize < 1) {
-                  await showDialog(
-                    context: context,
-                    builder: (context) {
-                      return CustomAlert(
-                        leadingIcon: Icon(MdiIcons.trashCan),
-                        title: Languages.of(context).labelCleaning,
-                        content: Languages.of(context).labelCacheIsEmpty,
-                        actions: <Widget>[
-                          FlatButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: Text("OK"),
-                          ),
-                        ],
-                      );
+            subtitle: Text(Languages.of(context).labelDeleteCacheJustification,
+                style: TextStyle(fontSize: 12)),
+            trailing: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Theme.of(context).scaffoldBackgroundColor),
+              child: IconButton(
+                icon: Icon(EvaIcons.trashOutline,
+                    color: Theme.of(context).iconTheme.color),
+                onPressed: () async {
+                  double totalSize = 0;
+                  String tmpPath = (await getTemporaryDirectory()).path;
+                  List<FileSystemEntity> listFiles =
+                      Directory(tmpPath).listSync();
+                  listFiles.forEach((element) {
+                    if (element is File) {
+                      totalSize += element.statSync().size;
                     }
-                  );
-                  return;
-                }
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return CustomAlert(
-                      leadingIcon: Icon(MdiIcons.trashCan),
-                      title: Languages.of(context).labelCleaning,
-                      content: Languages.of(context).labelYouAreAboutToClear +
-                        ": " + totalSize.toStringAsFixed(2) + "MB",
-                      actions: <Widget>[
-                        FlatButton(
-                          onPressed: () async {
-                            await Directory(tmpPath).delete(recursive: true);
-                            Navigator.pop(context);
-                          },
-                          child: Text("OK"),
-                        ),
-                        FlatButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Text(Languages.of(context).labelCancel),
-                        )
-                      ],
-                    );
+                  });
+                  totalSize = totalSize * 0.000001;
+                  if (totalSize < 1) {
+                    await showDialog(
+                        context: context,
+                        builder: (context) {
+                          return CustomAlert(
+                            leadingIcon: Icon(MdiIcons.trashCan),
+                            title: Languages.of(context).labelCleaning,
+                            content: Languages.of(context).labelCacheIsEmpty,
+                            actions: <Widget>[
+                              FlatButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text("OK"),
+                              ),
+                            ],
+                          );
+                        });
+                    return;
                   }
-                );
-              },
-            ), 
-          )
-        ),
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return CustomAlert(
+                          leadingIcon: Icon(MdiIcons.trashCan),
+                          title: Languages.of(context).labelCleaning,
+                          content:
+                              Languages.of(context).labelYouAreAboutToClear +
+                                  ": " +
+                                  totalSize.toStringAsFixed(2) +
+                                  "MB",
+                          actions: <Widget>[
+                            FlatButton(
+                              onPressed: () async {
+                                await Directory(tmpPath)
+                                    .delete(recursive: true);
+                                Navigator.pop(context);
+                              },
+                              child: Text("OK"),
+                            ),
+                            FlatButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text(Languages.of(context).labelCancel),
+                            )
+                          ],
+                        );
+                      });
+                },
+              ),
+            )),
       ],
     );
   }
