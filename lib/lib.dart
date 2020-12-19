ab7f53986cdfc411d4be7f506ad9cf1c926d896e
Clashkid155 <frankdiss9@gmail.com>
2020-12-19 04:44:26 +0100

    1. Pass package name and download path to app update dialog 2. VScode reformatting

--- lib/lib.dart
+++ lib/lib.dart
@@ -38,13 +38,9 @@ class MainLib extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Stack(
-        children: [
-          Lib(),
-          SlidingPlayerPanel()
-        ],
-      )
-    );
+        child: Stack(
+      children: [Lib(), SlidingPlayerPanel()],
+    ));
   }
 }
 
@@ -54,111 +50,110 @@ class Lib extends StatefulWidget {
 }
 
 class _LibState extends State<Lib> {
-
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.renderView.automaticSystemUiAdjustment=false;
+    WidgetsBinding.instance.renderView.automaticSystemUiAdjustment = false;
     KeyboardVisibility.onChange.listen((bool visible) {
-        if (visible == false) FocusScope.of(context).unfocus();
-      }
-    );
-    WidgetsBinding.instance.addObserver(
-      new LifecycleEventHandler(resumeCallBack: () async {
-        PreferencesProvider prefs = Provider.of<PreferencesProvider>(context, listen: false);
-        DownloadsProvider downloads = Provider.of<DownloadsProvider>(context, listen: false);
-        if (downloads.queueList.isNotEmpty ||
+      if (visible == false) FocusScope.of(context).unfocus();
+    });
+    WidgetsBinding.instance
+        .addObserver(new LifecycleEventHandler(resumeCallBack: () async {
+      PreferencesProvider prefs =
+          Provider.of<PreferencesProvider>(context, listen: false);
+      DownloadsProvider downloads =
+          Provider.of<DownloadsProvider>(context, listen: false);
+      if (downloads.queueList.isNotEmpty ||
           downloads.downloadingList.isNotEmpty ||
           downloads.convertingList.isNotEmpty ||
-          downloads.completedList.isNotEmpty
-        ) {
-          if (prefs.showJoinTelegramDialog && prefs.remindTelegramLater == false) {
-            showDialog<void>(
-              context: context,
-              builder: (_) => JoinTelegramDialog()
-            );
-          }
+          downloads.completedList.isNotEmpty) {
+        if (prefs.showJoinTelegramDialog &&
+            prefs.remindTelegramLater == false) {
+          showDialog<void>(
+              context: context, builder: (_) => JoinTelegramDialog());
         }
-        String intent = await NativeMethod.handleIntent();
-        if (intent == null) return;
-        if (VideoId.parseVideoId(intent) != null) {
-          String id = VideoId.parseVideoId(intent);
-          showDialog(
-            context: context,
-            builder: (_) => LoadingDialog()
-          );
-          YoutubeExplode yt = YoutubeExplode();
-          Video video = await yt.videos.get(id);
-          Provider.of<ManagerProvider>(context, listen: false)
+      }
+      String intent = await NativeMethod.handleIntent();
+      if (intent == null) return;
+      if (VideoId.parseVideoId(intent) != null) {
+        String id = VideoId.parseVideoId(intent);
+        showDialog(context: context, builder: (_) => LoadingDialog());
+        YoutubeExplode yt = YoutubeExplode();
+        Video video = await yt.videos.get(id);
+        Provider.of<ManagerProvider>(context, listen: false)
             .updateMediaInfoSet(video, null);
-          Navigator.pop(context);
-          Navigator.push(context,
+        Navigator.pop(context);
+        Navigator.push(
+            context,
             BlurPageRoute(
-              blurStrength: Provider.of<PreferencesProvider>(context, listen: false)
-                .enableBlurUI ? 20 : 0,
-              slideOffset: Offset(0.0, 10.0),
-              builder: (_) => YoutubePlayerVideoPage(
-                url: video.id.value,
-                thumbnailUrl: video.thumbnails.highResUrl,
-              )
-          ));
-        }
-        if (PlaylistId.parsePlaylistId(intent) != null) {
-          String id = PlaylistId.parsePlaylistId(intent);
-          showDialog(
-            context: context,
-            builder: (_) => LoadingDialog()
-          );
-          YoutubeExplode yt = YoutubeExplode();
-          Playlist playlist = await yt.playlists.get(id);
-          Provider.of<ManagerProvider>(context, listen: false)
+                blurStrength:
+                    Provider.of<PreferencesProvider>(context, listen: false)
+                            .enableBlurUI
+                        ? 20
+                        : 0,
+                slideOffset: Offset(0.0, 10.0),
+                builder: (_) => YoutubePlayerVideoPage(
+                      url: video.id.value,
+                      thumbnailUrl: video.thumbnails.highResUrl,
+                    )));
+      }
+      if (PlaylistId.parsePlaylistId(intent) != null) {
+        String id = PlaylistId.parsePlaylistId(intent);
+        showDialog(context: context, builder: (_) => LoadingDialog());
+        YoutubeExplode yt = YoutubeExplode();
+        Playlist playlist = await yt.playlists.get(id);
+        Provider.of<ManagerProvider>(context, listen: false)
             .updateMediaInfoSet(playlist, null);
-          Navigator.pop(context);
-          Navigator.push(context,
+        Navigator.pop(context);
+        Navigator.push(
+            context,
             BlurPageRoute(
-              blurStrength: Provider.of<PreferencesProvider>(context, listen: false)
-                .enableBlurUI ? 20 : 0,
-              slideOffset: Offset(0.0, 10.0),
-              builder: (_) => YoutubePlayerPlaylistPage()
-          ));
-        }
-        return;
-      })
-    );
+                blurStrength:
+                    Provider.of<PreferencesProvider>(context, listen: false)
+                            .enableBlurUI
+                        ? 20
+                        : 0,
+                slideOffset: Offset(0.0, 10.0),
+                builder: (_) => YoutubePlayerPlaylistPage()));
+      }
+      return;
+    }));
     Provider.of<MediaProvider>(context, listen: false).loadSongList();
     Provider.of<MediaProvider>(context, listen: false).loadVideoList();
     // Disclaimer
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       // Show Disclaimer
-      if (!Provider.of<ConfigurationProvider>(context, listen: false).disclaimerAccepted) {
+      if (!Provider.of<ConfigurationProvider>(context, listen: false)
+          .disclaimerAccepted) {
         await showDialog(
-          context: context,
-          builder: (context) => DisclaimerDialog()
-        );
+            context: context, builder: (context) => DisclaimerDialog());
       }
-      if (Provider.of<ConfigurationProvider>(context, listen: false).showDownloadFixDialog) {
+      if (Provider.of<ConfigurationProvider>(context, listen: false)
+          .showDownloadFixDialog) {
         AndroidDeviceInfo deviceInfo = await DeviceInfoPlugin().androidInfo;
         int sdkNumber = deviceInfo.version.sdkInt;
         if (sdkNumber >= 30) {
           await showDialog(
-            context: context,
-            builder: (context) => DownloadFixDialog()
-          );
+              context: context, builder: (context) => DownloadFixDialog());
         }
         Provider.of<ConfigurationProvider>(context, listen: false)
-          .showDownloadFixDialog = false;
+            .showDownloadFixDialog = false;
       }
       // Check for Updates
       PackageInfo.fromPlatform().then((android) {
-        double appVersion = double
-          .parse(android.version.replaceRange(3, 5, ""));
+        double appVersion =
+            double.parse(android.version.replaceRange(3, 5, ""));
         getLatestRelease().then((details) {
-          if (appVersion < details.version) {
+          if (appVersion == details.version) {
             // Show the user an Update is available
             showDialog(
-              context: context,
-              builder: (context) => AppUpdateDialog(details)
-            );
+                context: context,
+                builder: (context) => AppUpdateDialog(
+                      details,
+                      Provider.of<ConfigurationProvider>(context)
+                          .videoDownloadPath,
+                      Provider.of<ConfigurationProvider>(context).packageName,
+                    ));
           }
         });
       });
@@ -171,8 +166,8 @@ class _LibState extends State<Lib> {
     MediaProvider mediaProvider = Provider.of<MediaProvider>(context);
     Brightness _systemBrightness = Theme.of(context).brightness;
     Brightness _statusBarBrightness = _systemBrightness == Brightness.light
-      ? Brightness.dark
-      : Brightness.light;
+        ? Brightness.dark
+        : Brightness.light;
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
@@ -191,15 +186,15 @@ class _LibState extends State<Lib> {
       child: Scaffold(
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         resizeToAvoidBottomInset: false,
-        key: Provider.of<ManagerProvider>
-          (context, listen: false).libraryScaffoldKey,
+        key: Provider.of<ManagerProvider>(context, listen: false)
+            .libraryScaffoldKey,
         body: Container(
           color: Theme.of(context).cardColor,
           child: SafeArea(
             child: WillPopScope(
               onWillPop: () {
                 ManagerProvider manager =
-                  Provider.of<ManagerProvider>(context, listen: false);
+                    Provider.of<ManagerProvider>(context, listen: false);
                 if (mediaProvider.slidingPanelOpen) {
                   mediaProvider.slidingPanelOpen = false;
                   mediaProvider.panelController.close();
@@ -207,40 +202,36 @@ class _LibState extends State<Lib> {
                 } else if (manager.showSearchBar) {
                   manager.showSearchBar = false;
                   return Future.value(false);
-                } else if (manager.screenIndex == 0 && manager.currentHomeTab != HomeScreenTab.Home) {
+                } else if (manager.screenIndex == 0 &&
+                    manager.currentHomeTab != HomeScreenTab.Home) {
                   manager.currentHomeTab = HomeScreenTab.Home;
                   return Future.value(false);
                 } else {
                   return Future.value(true);
                 }
               },
-              child: Consumer<ManagerProvider>(
-                builder: (context, manager, _) {
-                  return Column(
-                    children: [
-                      Expanded(
-                        child: AnimatedSwitcher(
+              child: Consumer<ManagerProvider>(builder: (context, manager, _) {
+                return Column(
+                  children: [
+                    Expanded(
+                      child: AnimatedSwitcher(
                           duration: Duration(milliseconds: 250),
-                          child: _currentScreen(manager)
-                        ),
-                      ),
-                      if (manager.screenIndex == 2)
+                          child: _currentScreen(manager)),
+                    ),
+                    if (manager.screenIndex == 2)
                       MusicPlayerPadding(manager.showSearchBar)
-                    ],
-                  );
-                }
-              ),
+                  ],
+                );
+              }),
             ),
           ),
         ),
-        bottomNavigationBar: Consumer<ManagerProvider>(
-          builder: (context, manager, _) {
-            return AppBottomNavigationBar(
+        bottomNavigationBar:
+            Consumer<ManagerProvider>(builder: (context, manager, _) {
+          return AppBottomNavigationBar(
               onItemTap: (int index) => manager.screenIndex = index,
-              currentIndex: manager.screenIndex
-            );
-          }
-        ),
+              currentIndex: manager.screenIndex);
+        }),
       ),
     );
   }
@@ -258,5 +249,4 @@ class _LibState extends State<Lib> {
       return Container();
     }
   }
-  
 }
