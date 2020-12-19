a5e9b129d7e747cbed730abc24d07055fe916de1
Clashkid155 <frankdiss9@gmail.com>
2020-12-19 04:50:47 +0100

    1. Add apk arch to update class 2. Get links of apk (parse)

--- lib/internal/models/updateDetails.dart
+++ lib/internal/models/updateDetails.dart
@@ -1,13 +1,16 @@
 class UpdateDetails {
-
   double version;
   String publishDate;
   String updateDetails;
+  String arm;
+  String arm64;
+  String general;
+  String x86;
 
-  UpdateDetails(
-    this.version,
-    this.publishDate,
-    this.updateDetails
-  );
-
+  UpdateDetails(this.version, this.publishDate, this.updateDetails, this.arm,
+      this.arm64, this.general, this.x86);
+  @override
+  String toString() {
+    return "{Version: $version, publishDate: $publishDate,\n arm: $arm,\n arm64: $arm64,\n updateDetails: $updateDetails}";
+  }
 }
--- lib/internal/updateChecker.dart
+++ lib/internal/updateChecker.dart
@@ -6,18 +6,24 @@ Future<UpdateDetails> getLatestRelease() async {
   try {
     var client = http.Client();
     var headers = {
-      "Accept": "application/vnd.github.v3+json"
-    };
-    var response = await client
-      .get("https://api.github.com/repos/SongTube/SongTube-App/releases",
+      "Accept": "application/vnd.github.v3+json",
+ };
+    var response = await client.get(
+        "https://api.github.com/repos/SongTube/SongTube-App/releases",
         headers: headers);
     var jsonResponse = jsonDecode(response.body);
     UpdateDetails details = UpdateDetails(
-      double.parse(jsonResponse[0]["tag_name"]
-        .split("+").first.trim().replaceRange(3, 5, "")),
-      jsonResponse[0]["published_at"].split("T").first,
-      jsonResponse[0]["body"]
-    );
+        double.parse(jsonResponse[0]["tag_name"]
+            .split("+")
+            .first
+            .trim()
+            .replaceRange(3, 5, "")),
+        jsonResponse[0]["published_at"].split("T").first,
+        jsonResponse[0]["body"],
+        jsonResponse[0]["assets"][0]["browser_download_url"],
+        jsonResponse[0]["assets"][1]["browser_download_url"],
+        jsonResponse[0]["assets"][2]["browser_download_url"],
+        jsonResponse[0]["assets"][3]["browser_download_url"]);
     client.close();
     return details;
   } catch (_) {
@@ -25,6 +31,4 @@ Future<UpdateDetails> getLatestRelease() async {
   }
 }
 
-Future<void> queryUpdate() async {
-
-}
+Future<void> queryUpdate() async {}
