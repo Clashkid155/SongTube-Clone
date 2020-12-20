05a0c696a9794eb2435ce779703bbbc48f149879
Clashkid155 <frankdiss9@gmail.com>
2020-12-20 01:45:55 +0100

    Parse Changelog to get latest

---
+++ changeprase.py
@@ -0,0 +1,15 @@
+with open("changelog.md", "r") as file:
+    a = 1
+    li = []
+    fake = []
+    body = ""
+    for i in file:
+        a+=1
+        if i.startswith("#"):
+            li.append(a)
+        if len(li) == 1:
+            fake.append(i)
+#           print(i, end="")
+    body = body.join(fake)
+    print(f"::set-env name=BODY::{body}")
+
